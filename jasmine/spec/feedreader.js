69ffbd8854ffab0a36188dfc0ee3b2e35a88456e
Linesh <linesh.simpcity@gmail.com>
2017-05-24 21:27:45 +0800

    [Linesh] 🚨Test: remove unnecessary comments as they are explained in suite description

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,10 +26,6 @@ $(function () {
       expect(allFeeds.length).not.toBe(0);
     });
 
-    /*
-     * in the allFeeds object and ensures it has a URL defined
-     * and that the URL is not empty.
-     */
     it('all elements in allFeeds should have a url property', () => {
       const expected = [
         'http://blog.udacity.com/feed',
@@ -43,11 +39,6 @@ $(function () {
       });
     });
 
-
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a name defined
-     * and that the name is not empty.
-     */
     it('all elements in allFeeds should have a name property', () => {
       const expected = [
         'Udacity Blog',
@@ -62,9 +53,9 @@ $(function () {
     })
   });
 
+  describe('The menu', () => {
 
-  /* TODO: Write a new test suite named "The menu" */
-
+  })
   /* TODO: Write a test that ensures the menu element is
    * hidden by default. You'll have to analyze the HTML and
    * the CSS to determine how we're performing the
