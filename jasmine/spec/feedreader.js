b517c1888f38d6926fc36aaec73ab1e2b133efdf
Linesh <linesh.simpcity@gmail.com>
2017-05-25 10:02:55 +0800

    [Linesh] 🚨Test: write a test suite named 'Initial Entries'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -76,7 +76,10 @@ $(function () {
     })
   })
 
-  /* TODO: Write a new test suite named "Initial Entries" */
+  describe('Initial Entries', () => {
+
+  })
+
 
   /* TODO: Write a test that ensures when the loadFeed
    * function is called and completes its work, there is at least
